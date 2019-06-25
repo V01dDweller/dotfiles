93fb5d95473de394f5535117221f64a96c024340
Karl Simpson <karl.simpson@cesicorp.com>
2019-06-25 11:11:14 -0400

    Now installs pathogen, installs/updates plugins

--- vim/plugin_reinstall.sh
+++ vim/plugin_reinstall.sh
@@ -8,11 +8,57 @@
 #  vim-multiple-cursors - https://github.com/terryma/vim-multiple-cursors  
 #  vim-ps1              - https://github.com/PProvost/vim-ps1              
 
-if [ -d ~/.vim/bindle ]; then
+# Install pathogen
+if [ ! -d ~/.vim/autoload ]
+then
+  mkdir -pv ~/.vim/autoload
+fi 
+
+if [ ! -d ~/.vim/bundle ]
+then
+  mkdir -pv ~/.vim/bundle
+fi
+
+export PLUGINS="w0rp/ale terryma/vim-multiple-cursors dpc/vim-minimap PProvost/vim-ps1 tpope/vim-fugitive"
+
+if [ ! -f ~/.vim/autoload/pathogen.vim ]
+then
+  echo "Installing pathogen"
+  curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+  echo " "
+else
+  echo "Pathogen is already installed"
+  echo " "
+fi
+
+if [ -d ~/.vim/bundle ]
+then
   cd ~/.vim/bundle
-  for i in w0rp/ale terryma/vim-multiple-cursors dpc/vim-minimap PProvost/vim-ps1 tpope/vim-fugitive
+  for i in $PLUGINS
   do
-    git clone https://github.com/$i.git
+    export PLUGIN_DIR="`echo $i | cut -d '/' -f 2`"
+    if [ ! -d $PLUGIN_DIR ];then
+      echo "Installing" $PLUGIN_DIR
+      git clone https://github.com/$i.git
+      echo " "
+    else
+      echo "Updating " $PLUGIN_DIR
+      cd $PLUGIN_DIR
+      git pull
+      cd ..
+      echo " "
+    fi
   done
-  git clone --recursive git://github.com/noah/vim256-color.git
+  if [ ! -d ~/.vim/bundle/vim256-color ]
+  then
+    echo "Installing vim256-color, this could take a minute..."
+    git clone --recursive git://github.com/noah/vim256-color.git
+    echo ""
+  else
+    echo "Updating vim2560color"
+    cd vim256-color
+    git pull
+    cd ..
+    echo " "
+  fi
 fi
