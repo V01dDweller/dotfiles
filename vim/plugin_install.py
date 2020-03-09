98ce4d40a60e38188a8308ca5a014cb5cd549e4f
V01dDweller <lucan88@msn.com>
2020-03-09 15:37:56 -0400

    Fun with colors

--- vim/plugin_install.py
+++ vim/plugin_install.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Set up Pathogen and install my Vim plugins """
 
 # Install/update my favorite plug-ins via pathogen/git
 
@@ -15,90 +16,104 @@
 #  Vinegar           - Better netrw?                  - https://github.com/tpope/vim-vinegar
 #  vim256-color      - Too many color schemes         - https://github.com/noah/vim256-color
 
-import sys
 import os
 import urllib.request
+
+# Thanks to https://ozzmaker.com/add-colour-to-text-in-python/
+# Output Colors
+BLACK = '\033[0;30;40m'
+RED = '\033[0;31;40m'
+GREEN = '\033[0;32;40m'
+YELLOW = '\033[0;33;40m'
+BLUE = '\033[0;34;40m'
+PURPLE = '\033[0;35;40m'
+CYAN = '\033[0;36;40m'
+WHITE = '\033[0;37;40m'
+RESET = '\033[0;37;40m'
 
 # Creating ~/.vim
 HOME_DIR = os.environ['HOME']
 VIM_DIR = (HOME_DIR + '/.vim')
 if not os.path.exists(VIM_DIR):
-  print('Creating ' + VIM_DIR)
-  os.mkdir(VIM_DIR)
+    print('Creating ' + CYAN + VIM_DIR + RESET + '\n')
+    os.mkdir(VIM_DIR)
 else:
-  print(VIM_DIR + ' already exists')
+    print(CYAN + VIM_DIR + RESET + ' already exists\n')
 
 # Installing the Pathogen plugn-in manager
-PATHOGEN_DIRS = [ 'autoload', 'bundle' ]
+PATHOGEN_DIRS = ['autoload', 'bundle']
 PATHOGEN_PLUGIN_FILE = (VIM_DIR + '/autoload/pathogen.vim')
+PATHOGEN_URL = 'https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
 
+print('Creating Pathogen directories')
 for i in PATHOGEN_DIRS:
-    PLUGIN_PATH=(VIM_DIR + '/' + i)
+    PLUGIN_PATH = (VIM_DIR + '/' + i)
     if not os.path.exists(PLUGIN_PATH):
-        print('Creating ' + PLUGIN_PATH)
+        print('Creating ' + CYAN + PLUGIN_PATH + RESET)
         os.mkdir(PLUGIN_PATH)
     else:
-        print(PLUGIN_PATH + ' already exists')
+        print(CYAN + PLUGIN_PATH + RESET + ' already exists\n' + RESET)
 
 if not os.path.exists(PATHOGEN_PLUGIN_FILE):
-    print('Installing Pathogen to ' + PATHOGEN_PLUGIN_FILE)
-    urllib.request.urlretrieve('https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim', PATHOGEN_PLUGIN_FILE)
+    print('Installing Pathogen to ' + CYAN + PATHOGEN_PLUGIN_FILE + RESET)
+    urllib.request.urlretrieve(PATHOGEN_URL, PATHOGEN_PLUGIN_FILE)
 else:
-    print(PATHOGEN_PLUGIN_FILE + ' already downloaded')
+    print(CYAN + PATHOGEN_PLUGIN_FILE + RESET + ' already downloaded\n' + RESET)
 
 # Installing/Updating plug-ins
 VIM_PLUGINS = [
-                'w0rp/ale',
-                'vim-scripts/AnsiEsc.vim',
-                'pearofducks/ansible-vim',
-                'chrisbra/Colorizer',
-                'airblade/vim-gitgutter',
-                'dpc/vim-minimap',
-                'PProvost/vim-ps1',
-                'tpope/vim-fugitive',
-                'tpope/vim-vinegar',
-                'juneedahamed/svnj.vim',
-                'vim-scripts/delview'
-              ]
+        'w0rp/ale',
+        'vim-scripts/AnsiEsc.vim',
+        'pearofducks/ansible-vim',
+        'chrisbra/Colorizer',
+        'airblade/vim-gitgutter',
+        'dpc/vim-minimap',
+        'PProvost/vim-ps1',
+        'tpope/vim-fugitive',
+        'tpope/vim-vinegar',
+        'juneedahamed/svnj.vim',
+        'vim-scripts/delview'
+        ]
 BUNDLE_DIR = (VIM_DIR + '/bundle')
-START_DIR=(os.getcwd())
+START_DIR = (os.getcwd())
 
-print('Plugins will be installed in ' + BUNDLE_DIR)
-print(' ')
+print('Plugins will be installed in ' + CYAN + BUNDLE_DIR + '\n' + RESET)
 
 if os.path.exists(BUNDLE_DIR):
     for i in VIM_PLUGINS:
         REPO_PARTS = i.split('/')
         os.chdir(BUNDLE_DIR)
         if not os.path.exists(REPO_PARTS[1]):
-            PLUGIN_REPO=('https://github.com/'+i+'.git')
-            print('Cloning ' + PLUGIN_REPO)
+            PLUGIN_REPO = ('https://github.com/'+i+'.git')
+            print('Checking out ' + GREEN + PLUGIN_REPO + RESET)
             os.system('git clone ' + PLUGIN_REPO)
             print(" ")
         else:
-            print('Checking for updates to ' + REPO_PARTS[1])
+            print('Checking for updates to ' + GREEN + REPO_PARTS[1] + RESET)
             os.chdir(REPO_PARTS[1])
             os.system('git pull')
             print(' ')
     os.chdir(START_DIR)
 
-COLOR_DIR=(VIM_DIR + '/vim256-colors')
-DESIRE_COLORS=''
+# Install vim256-color colorschemes
+COLOR_DIR = (BUNDLE_DIR + '/vim256-color')
+DESIRE_COLORS = ''
+PROMT_FOR_COLOR = 'Install Vim color schemes?  (skip if you\'re in a hurry) (y/n): '
 while DESIRE_COLORS not in ('Y', 'y', 'N', 'n'):
-    DESIRE_COLORS=input('Install Vim color schemes? (skip if you\'re in a hurry) (y/n): ')
+    DESIRE_COLORS = input(YELLOW + PROMT_FOR_COLOR + RESET)
     if DESIRE_COLORS in ('Y', 'y'):
         os.chdir(BUNDLE_DIR)
-        print('Yay! Color!')
+        print('Yay! Color!\n')
         if not os.path.exists(COLOR_DIR):
+            print('Cloning color schemes, this will take some time...')
             os.system('git clone --recursive git://github.com/noah/vim256-color.git ')
         else:
             print('Colors already installed, updating...')
-            os.chdir(BUNDLE_DIR + 'vim256-color')
+            os.chdir(COLOR_DIR)
             os.system('git pull')
-        break
     elif DESIRE_COLORS in ('N', 'n'):
         print('Another time, perhaps')
         break
     else:
-        print('Please answer "y" or "n"')
+        print(YELLOW + 'Please answer "y" or "n"' + RESET)
     os.chdir(START_DIR)
