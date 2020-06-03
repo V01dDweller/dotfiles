9b319e42385308fdcc35eb386f6e61011eb01d34
V01dDweller <lucan88@msn.com>
2020-06-03 13:05:15 -0400

    vim256-colors, svnj, minimap removed
    Now just grabbing a color schemes via curl

--- vim/plugin_install.sh
+++ vim/plugin_install.sh
@@ -7,15 +7,12 @@
 #  AnsiEsc           - Convert ASCII escapes to color - https://github.com/vim-scripts/AnsiEsc.vim
 #  Colorizer         - Colorize HTML color codes      - https://github.com/chrisbra/Colorizer
 #  git-gutter        - Shows a git diff in the gutter - https://github.com/airblade/vim-gitgutter
-#  Minimap           - A Sublime-like minimap         - https://github.com/dpc/vim-minimap
 #  nginx.vim         - Nginx conf file syntax         - https://github.com/chr4/nginx.vim
+#  numbertoggle       - Autoswitch number modes        - https://github.com/jeffkreeftmeijer/vim-numbertoggle
 #  PS1               - PowerShell syntax highlighting - https://github.com/PProvost/vim-ps1
 #  Fugitive          - Git integration                - https://github.com/tpope/vim-fugitive
-#  SVNJ              - SVN integration                - https://github.com/juneedahamed/svnj.vim
 #  Delview           - Delete saved views             - https://github.com/vim-scripts/delview
 #  Vinegar           - Better netrw?                  - https://github.com/tpope/vim-vinegar
-#  vim256-color      - Too many color schemes         - https://github.com/noah/vim256-color
-
 
 # Bash scripting tweaks credit:
 #   https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail
@@ -51,13 +48,12 @@ export PLUGINS="\
   pearofducks/ansible-vim \
   chrisbra/Colorizer \
   airblade/vim-gitgutter \
-  dpc/vim-minimap \
   PProvost/vim-ps1 \
   tpope/vim-fugitive\
   tpope/vim-vinegar\
-  juneedahamed/svnj.vim \
   vim-scripts/delview \
   chr4/nginx.vim \
+  jeffkreeftmeijer/vim-numbertoggle \
   "
 
 if [ -d ~/.vim/bundle ]
@@ -82,37 +78,31 @@ then
       echo " "
     fi
   done
-
-# Install vim256-color colorschemes
-  export colors=""
-  while [[ ! $colors =~ ([YyNn]) ]]
-  do
-    echo ""
-    read -r -n 1 -p "Install Vim color schemes? (hint: skip if you're in a hurry) (y/n): " colors
-    case $colors in
-      [Yy]* )
-        echo ""
-        echo "Got it, installing color schemes"
-        if [ ! -d ~/.vim/bundle/vim256-color ]
-        then
-          echo "Installing vim256-color, this will take a minute..."
-          git clone --recursive git://github.com/noah/vim256-color.git
-          echo ""
-        else
-          echo "Updating vim256-color"
-          cd vim256-color
-          git pull
-          cd ..
-          echo " "
-        fi
-        ;;
-      [Nn]* )
-        echo ""
-        echo "No problem, maybe later"
-        ;;
-      *)
-        echo ""
-        echo "Press 'y' or 'n'"
-    esac
-  done
 fi
+
+# Install colorschemes from Github
+if [ ! -d ~/.vim/colors ]
+then
+  echo "Creating ~/.vim/colors"
+  mkdir ~/.vim/colors
+fi
+
+COLOR_SCHEMES=(
+  "/vim-scripts/xoria256.vim/master/colors/xoria256.vim"
+  "/jalvesaq/southernlights/master/colors/southernlights.vim"
+  "/dracula/vim/master/colors/dracula.vim"
+  "/morhetz/gruvbox/master/colors/gruvbox.vim"
+  "/gosukiwi/vim-atom-dark/master/colors/atom-dark-256.vim"
+  "/baskerville/bubblegum/master/colors/bubblegum-256-dark.vim"
+  "/micke/vim-hybrid/master/colors/hybrid.vim"
+  )
+
+for i in "${COLOR_SCHEMES[@]}"
+do
+  COLOR_FILE=$(basename "$i")
+  if [ ! -f "$HOME"/.vim/colors/"$COLOR_FILE" ]
+  then
+    echo "Retrieving $COLOR_FILE"
+    curl -s -o "$HOME"/.vim/colors/"$COLOR_FILE" https://raw.githubusercontent.com"$i"
+  fi
+done
