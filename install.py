5ba79cec937a822a606ffeb0ed3f2daddcf4c7d3
V01dDweller <lucan88@msn.com>
2020-12-29 16:41:00 +0000

    Clean up and add tmux check and file copy

--- install.py
+++ install.py
@@ -13,36 +13,53 @@
     5. Replace or create .bash, .vim and .tmux rc files from the lists below
     6. Run ~/dotfiles/tmux/plugin_install.sh to set up Tmux plug-ins
     7. Run ~/dotfiles/vim/plugin_install.sh to set up Vim plug-ins
+
 """
 
 import os
+import subprocess
 from datetime import datetime
+
+
+class color:
+    RED = '\u001b[31;1m'
+    GREEN = '\u001b[32;1m'
+    YELLOW = '\u001b[33;1m'
+    BLUE = '\u001b[34;1m'
+    MAGENTA = '\u001b[35;1m'
+    CYAN = '\u001b[36;1m'
+    WHITE = '\u001b[37;1m'
+    RESET = '\u001b[0m'
 
 # Setup for a bash git-aware prompt
 START_PATH = (os.getcwd())
 REPO_PATH = (os.path.dirname(os.path.realpath(__file__)))
 HOME_DIR = os.environ['HOME']
-BASH_PATH = (HOME_DIR + './bash')
+BASH_PATH = (HOME_DIR + '/.bash')
 GITAWARE_PATH = (BASH_PATH + '/git-aware-prompt')
 GITAWARE_REPO = 'git://github.com/jimeh/git-aware-prompt.git'
 
 if os.path.exists(BASH_PATH):
-    print('Found ~/.bash, checking for ~/.bash/git-aware-prompt')
+    print(color.GREEN +
+          'Found ~/.bash, checking for ~/.bash/git-aware-prompt...' +
+          color.RESET)
     if os.path.exists(GITAWARE_PATH):
-        print('Found the git-aware-prompt, updating...')
+        print(color.GREEN + 'Found the git-aware-prompt, updating...' +
+              color.RESET)
         os.chdir(GITAWARE_PATH)
         os.system('git pull')
         print(' ')
         os.chdir(START_PATH)
     else:
-        print('Didn\'t find git-aware-prompt,')
+        print(color.YELLOW + 'Didn\'t find git-aware-prompt,' + color.RESET)
         print('Cloning it now')
         os.chdir(BASH_PATH)
         os.system('git clone --depth 1' + GITAWARE_REPO)
         os.chdir(START_PATH)
         print(' ')
 else:
-    print('Didn\'t find ~/.bash, creating it now...')
+    print(color.YELLOW + 'Didn\'t find ~/.bash, creating it now...' +
+          color.RESET)
     os.mkdir(BASH_PATH)
     os.chdir(BASH_PATH)
     os.system('git clone --depth 1' + GITAWARE_REPO)
@@ -55,41 +72,53 @@ TIME_STAMP = NOW.strftime('%Y-%m-%d_%H:%M:%S')
 
 # The dotfiles
 BASH_FILES = [
-        '.bashrc',
-        '.bash_profile',
-        '.bash_prompt.sh',
-        '.LESS_TERMCAP'
-        ]
+    '.bashrc',
+    '.bash_profile',
+    '.bash_prompt.sh',
+    '.LESS_TERMCAP'
+    ]
 MINTTY_FILES = [
-        '.minttyrc',
-        '.bash_mintty_colors'
-        ]
+    '.minttyrc',
+    '.bash_mintty_colors'
+    ]
 VIM_FILES = [
-        '.vimrc',
-        '.gvimrc'
-        ]
+    '.vimrc',
+    '.gvimrc'
+    ]
 TMUX_FILES = [
-        '.tmux.conf',
-        '.tmux-syncoff.conf',
-        '.tmux-syncon.conf',
-        '.tmux.clipboard.conf',
-        '.tmux-status.conf'
-        ]
+    '.tmux.conf',
+    '.tmux-syncoff.conf',
+    '.tmux-syncon.conf',
+    '.tmux.clipboard.conf',
+    '.tmux-status.conf'
+    ]
 BACKUP_FILES = BASH_FILES + MINTTY_FILES + VIM_FILES + TMUX_FILES
 BACKUP_DIRECTORY = HOME_DIR + '/dotfile_backup_' + TIME_STAMP
 
 # Dotfile backup
-print('Creating ' + BACKUP_DIRECTORY)
+print(color.GREEN + 'Creating ' + BACKUP_DIRECTORY + color.RESET)
 os.mkdir(BACKUP_DIRECTORY)
 for i in BACKUP_FILES:
     if os.path.exists(HOME_DIR + '/' + i):
         CURRENT_FILE = HOME_DIR + '/' + i
-        print('Backing up ' + i)
+        print(color.GREEN + 'Backing up ' + i + color.RESET)
         os.system('cp -v ' + CURRENT_FILE + ' ' + BACKUP_DIRECTORY)
     print(' ')
 
 # Setting up bash
-print('Setting up bash')
+print(color.GREEN + 'Setting up bash' + color.RESET)
 for i in BASH_FILES:
-    print('Creating ' + HOME_DIR + "/" + i)
-    os.system('cp -v ' + REPO_PATH + '/bash/' + i + " " + HOME_DIR + '/' + i)
+    print(color.GREEN + 'Creating ' + HOME_DIR + '/' + i + color.RESET)
+    os.system('cp -v ' + REPO_PATH + '/bash/' + i + ' ' + HOME_DIR + '/' + i)
+
+# Setting up tmux
+tmux_check = subprocess.getstatusoutput('which tmux')
+if (tmux_check[0] == 0):
+    print(color.GREEN + 'Copying tmux dot files' + color.RESET)
+    for i in TMUX_FILES:
+        print(color.GREEN + 'Creating ' + HOME_DIR + '/' + i + color.RESET)
+        os.system('cp -v ' + REPO_PATH + '/tmux/' + i + ' ' +
+                  HOME_DIR + '/' + i)
+    print (' ')
+    print(color.YELLOW + '** Reminder: Uncomment the appropriate block in ' +
+          HOME_DIR + '/.tmux.conf for clipboard integration' + color.RESET)
