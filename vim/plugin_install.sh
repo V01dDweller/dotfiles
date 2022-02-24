f59446376e48709cdd5c3bc8e8804a8a9a0cc2fb
V01dDweller <lucan88@msn.com>
2022-02-24 09:58:58 -0500

    Remove the loupe plugin

--- vim/plugin_install.sh
+++ vim/plugin_install.sh
@@ -14,7 +14,6 @@
 # - Git Gutter                     - https://github.com/airblade/vim-gitgutter
 # - Jenkinsfile syntax.            - https://github.com/martinda/Jenkinsfile-vim-syntax
 # - Lion                           - https://github.com/tommcdo/vim-lion
-# - Loupe                          - https://github.com/wincent/loupe
 # - NERDTree                       - https://github.com/preservim/nerdtree
 # - Nginx conf file syntax         - https://github.com/chr4/nginx.vim
 # - PS1 syntax for PowerShell      - https://github.com/PProvost/vim-ps1
@@ -68,7 +67,6 @@ export PLUGINS="\
   chr4/nginx.vim \
   dracula/vim \
   tommcdo/vim-lion \
-  wincent/loupe \
   preservim/nerdtree \
   edkolev/tmuxline.vim \
   edkolev/promptline.vim \
