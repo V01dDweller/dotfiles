b9ee2479b0f3c4811bd55f07d68219c56530d0b6
V01dDweller <lucan88@msn.com>
2022-11-06 14:13:52 -0500

    Default to matching 'minimalist' AirLine theme

--- bash/.bash_prompt.sh
+++ bash/.bash_prompt.sh
@@ -59,67 +59,6 @@ function __promptline_vcs_branch {
   fi
   return 1
 }
-function __promptline_cwd {
-  local dir_limit="2"
-  local truncation="⋯"
-  local first_char
-  local part_count=0
-  local formatted_cwd=""
-  local dir_sep="/"
-  local tilde="~"
-
-  local cwd="${PWD/#$HOME/$tilde}"
-
-  # get first char of the path, i.e. tilde or slash
-  [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
-
-  # remove leading tilde
-  cwd="${cwd#\~}"
-
-  while [[ "$cwd" == */* && "$cwd" != "/" ]]; do
-    # pop off last part of cwd
-    local part="${cwd##*/}"
-    cwd="${cwd%/*}"
-
-    formatted_cwd="$dir_sep$part$formatted_cwd"
-    part_count=$((part_count+1))
-
-    [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
-  done
-
-  printf "%s" "$first_char$formatted_cwd"
-}
-function __promptline_left_prompt {
-  local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
-
-  # section "a" header
-  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "a" slices
-  __promptline_wrapper "\u" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "b" header
-  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "b" slices
-  __promptline_wrapper "\h" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # section "c" header
-  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
-  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
-  # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
-
-  # close sections
-  printf "%s" "${reset_bg}${sep}$reset$space"
-}
-function __promptline_wrapper {
-  # wrap the text in $1 with $2 and $3, only if $1 is not empty
-  # $2 and $3 typically contain non-content-text, like color escape codes and separators
-
-  [[ -n "$1" ]] || return 1
-  printf "%s" "${2}${1}${3}"
-}
 function __promptline_git_status {
   [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]] || return 1
 
@@ -169,6 +108,37 @@ function __promptline_git_status {
   [[ $has_untracked_files -gt 0 ]] && { printf "%s" "$leading_whitespace$has_untracked_files_symbol"; leading_whitespace=" "; }
   [[ $is_clean -gt 0 ]]            && { printf "%s" "$leading_whitespace$clean_symbol"; leading_whitespace=" "; }
 }
+function __promptline_left_prompt {
+  local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
+
+  # section "a" header
+  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "a" slices
+  __promptline_wrapper "\@" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "b" header
+  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "b" slices
+  __promptline_wrapper "\h" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "c" header
+  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "c" slices
+  __promptline_wrapper "\W" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # close sections
+  printf "%s" "${reset_bg}${sep}$reset$space"
+}
+function __promptline_wrapper {
+  # wrap the text in $1 with $2 and $3, only if $1 is not empty
+  # $2 and $3 typically contain non-content-text, like color escape codes and separators
+
+  [[ -n "$1" ]] || return 1
+  printf "%s" "${2}${1}${3}"
+}
 function __promptline_right_prompt {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix
 
@@ -205,21 +175,21 @@ function __promptline {
   local alt_rsep=""
   local reset="${wrap}0${end_wrap}"
   local reset_bg="${wrap}49${end_wrap}"
-  local a_fg="${wrap}38;5;17${end_wrap}"
-  local a_bg="${wrap}48;5;190${end_wrap}"
-  local a_sep_fg="${wrap}38;5;190${end_wrap}"
-  local b_fg="${wrap}38;5;255${end_wrap}"
-  local b_bg="${wrap}48;5;238${end_wrap}"
-  local b_sep_fg="${wrap}38;5;238${end_wrap}"
-  local c_fg="${wrap}38;5;85${end_wrap}"
-  local c_bg="${wrap}48;5;234${end_wrap}"
-  local c_sep_fg="${wrap}38;5;234${end_wrap}"
-  local warn_fg="${wrap}38;5;232${end_wrap}"
-  local warn_bg="${wrap}48;5;166${end_wrap}"
-  local warn_sep_fg="${wrap}38;5;166${end_wrap}"
-  local y_fg="${wrap}38;5;255${end_wrap}"
-  local y_bg="${wrap}48;5;238${end_wrap}"
-  local y_sep_fg="${wrap}38;5;238${end_wrap}"
+  local a_fg="${wrap}38;5;254${end_wrap}"
+  local a_bg="${wrap}48;5;237${end_wrap}"
+  local a_sep_fg="${wrap}38;5;237${end_wrap}"
+  local b_fg="${wrap}38;5;254${end_wrap}"
+  local b_bg="${wrap}48;5;239${end_wrap}"
+  local b_sep_fg="${wrap}38;5;239${end_wrap}"
+  local c_fg="${wrap}38;5;255${end_wrap}"
+  local c_bg="${wrap}48;5;235${end_wrap}"
+  local c_sep_fg="${wrap}38;5;235${end_wrap}"
+  local warn_fg="${wrap}38;5;234${end_wrap}"
+  local warn_bg="${wrap}48;5;215${end_wrap}"
+  local warn_sep_fg="${wrap}38;5;215${end_wrap}"
+  local y_fg="${wrap}38;5;254${end_wrap}"
+  local y_bg="${wrap}48;5;239${end_wrap}"
+  local y_sep_fg="${wrap}38;5;239${end_wrap}"
   if [[ -n ${ZSH_VERSION-} ]]; then
     PROMPT="$(__promptline_left_prompt)"
     RPROMPT="$(__promptline_right_prompt)"
@@ -230,7 +200,7 @@ function __promptline {
       __promptline_ps1
     fi
   else
-    PS1="$(__promptline_ps1) "
+    PS1="$(__promptline_ps1)"
   fi
 }
 
