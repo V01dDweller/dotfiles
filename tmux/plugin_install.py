caeb166a0eeefa587e0acc81f624d30e96b79868
V01dDweller <lucan88@msn.com>
2023-07-22 13:57:31 -0400

    Colorize print messages

--- tmux/plugin_install.py
+++ tmux/plugin_install.py
@@ -22,12 +22,12 @@ PLUGINS_DIR = TMUX_DIR + '/plugins'
 START_DIR = os.getcwd()
 
 if not os.path.exists(PLUGINS_DIR):
-    print('Creating ' + TMUX_DIR + '\n')
+    print(COLOR["green"] + 'Creating ' + TMUX_DIR + '\n' + COLOR["reset"])
     os.mkdir(TMUX_DIR)
     os.mkdir(PLUGINS_DIR)
     print(" ")
 else:
-    print(PLUGINS_DIR + ' already exists\n')
+    print(COLOR["green"] + PLUGINS_DIR + ' already exists\n' + COLOR["reset"])
     print(" ")
 
 # Clone/update Tmux plugin manager
@@ -35,10 +35,10 @@ TPM_REPO_URL = 'https://github.com/tmux-plugins/tpm/'
 TPM_DIR = TMUX_DIR + '/plugins/tpm'
 
 if not os.path.exists(TPM_DIR):
-    print('Cloning tpm (Tmux plugin manager) from GitHub\n')
+    print(COLOR["green"] + 'Cloning tpm (Tmux plugin manager) from GitHub\n' + COLOR["reset"])
     os.system('git clone --depth 1 ' + TPM_REPO_URL + ' ' + TPM_DIR)
 else:
-    print('Pulling tpm updates\n')
+    print(COLOR["green"] + 'Pulling tpm updates\n' + COLOR["reset"])
     os.chdir(TPM_DIR)
     os.system('git pull')
     os.chdir(START_DIR)
@@ -48,11 +48,11 @@ else:
 CLUSTER_REPO_URL = 'https://github.com/davidscholberg/tmux-cluster.git'
 CLUSTER_DIR = TMUX_DIR + '/plugins/tmux-cluster'
 if not os.path.exists(CLUSTER_DIR):
-    print('Cloning tmux-cluster from GitHub\n')
+    print(COLOR["green"] + 'Cloning tmux-cluster from GitHub\n' + COLOR["reset"])
     os.system('git clone --depth 1 ' + CLUSTER_REPO_URL + ' ' + CLUSTER_DIR)
     print(" ")
 else:
-    print('Pulling tmux-cluster updates\n')
+    print(COLOR["green"] + 'Pulling tmux-cluster updates\n' + COLOR["reset"])
     os.chdir(CLUSTER_DIR)
     os.system('git pull')
     os.chdir(START_DIR)
@@ -61,20 +61,20 @@ else:
 # Create $HOME/.local/bin
 LOCAL_BIN_DIR = HOME_DIR + '/.local/bin'
 if not os.path.exists(LOCAL_BIN_DIR):
-    print('Creating ' + LOCAL_BIN_DIR + '\n')
+    print(COLOR["green"] + 'Creating ' + LOCAL_BIN_DIR + '\n' + COLOR["reset"])
     os.mkdir(LOCAL_BIN_DIR)
     print(" ")
 else:
-    print(LOCAL_BIN_DIR + ' already exists\n')
+    print(COLOR["green"] + LOCAL_BIN_DIR + ' already exists\n' + COLOR["reset"])
     print(" ")
 
 # Downloading tmc script
 TMC_SCRIPT_URL = 'https://raw.githubusercontent.com/davidscholberg/tmux-cluster/master/tmc'
 TMC_SCRIPT = LOCAL_BIN_DIR + '/tmc'
 if not os.path.exists(TMC_SCRIPT):
-    print('Downloading tmc script\n')
+    print(COLOR["green"] + 'Downloading tmc script\n' + COLOR["reset"])
     os.system('wget ' + TMC_SCRIPT_URL + ' -O ' + TMC_SCRIPT)
     os.system('chmod +x ' + TMC_SCRIPT)
     print(" ")
 else:
-    print('tmc script already installed\n')
+    print(COLOR["green"] + 'tmc script already installed\n' + COLOR["reset"])
